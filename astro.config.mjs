a7f603465d29cabd32d4e403b634342946401b11
davecodescode <daveikumbu@gmail.com>
2026-05-29 18:11:58 +0300

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://neprotimber.co.ke',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
